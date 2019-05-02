492742cbed202d4f9a52d5f45b77e73548a1049b
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-05-02 17:32:59 -0600

    Want to put resume in a modal. Working on it.

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react'
+import { 
+  Modal, 
+  Button, 
+  ModalBody,
+  ModalFooter
+ } from 'reactstrap'
+ import resume from '../assets/T.OlsenMay2.pdf'
 import "./index.css"
 
 export class Index extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  }
   render() {
     return (
       <div>
@@ -13,12 +35,12 @@ export class Index extends Component {
           </div>
         </div>
 
-        <section class="section section-light">
+        <section className="section section-light">
           <h2>Section 1</h2>
           <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</p>
         </section>
 
-        <div className="pimg2">
+        <div className="pimg2 trans">
           <div className="ptext">
             <span className="border">
               Image two text
@@ -26,12 +48,12 @@ export class Index extends Component {
           </div>
         </div>
 
-        <section class="section section-dark">
+        <section className="section section-dark">
           <h2>Section 2</h2>
           <p>The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me? " he thought. It wasn't a dream.</p>
         </section>
 
-        <div className="pimg3">
+        <div className="pimg3 trans">
           <div className="ptext">
             <span className="border">
               Image three text
@@ -39,11 +61,28 @@ export class Index extends Component {
           </div>
         </div>
 
-        <section class="section section-dark">
+        <section className="section section-dark">
           <h2>Section 3</h2>
           <p>His room, a proper human room although a little too small, lay peacefully between its four familiar walls.<br/>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame.</p>
         </section>
 
+        <div className="pimg4 trans">
+          <div className="ptext">
+            <span className="border">
+              <Button onClick={this.toggle}>
+                Resume                
+              </Button>
+              <Modal id="myModal" class="modal" toggle={this.toogle}>
+                <ModalBody>
+                <img src={resume} alt="?"/>
+                </ModalBody>
+                <ModalFooter>
+                  <Button onClick={this.toggle}>X</Button>
+                </ModalFooter>
+              </Modal>
+            </span>
+          </div>
+        </div>
 
 
       </div>
